bb6026fbabc3bf012ee50856e1b477539d120bb3
user <jeon6213@naver.com>
2022-09-27 19:22:06 +0900

    mvvm테스트

--- app/src/main/java/com/cookandroid/mvvmtest/MainActivity.kt
+++ app/src/main/java/com/cookandroid/mvvmtest/MainActivity.kt
@@ -2,10 +2,16 @@ package com.cookandroid.mvvmtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //mvvm_text.setOnclickLisner()
+
+
+
     }
 }
